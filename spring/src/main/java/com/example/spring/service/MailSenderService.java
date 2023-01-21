94d54f4a43ff6bb224cb440f375fc64b10bde596
AlexRailstone <96865953+AlexRailstone@users.noreply.github.com>
2023-01-21 11:56:25 +0300

    Commit message mail

---
+++ spring/src/main/java/com/example/spring/service/MailSenderService.java
@@ -0,0 +1,30 @@
+package com.example.spring.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailSenderService {
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    public MailSenderService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+
+    public void send(String mailTo, String subject, String message) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+        mailMessage.setFrom(username);
+        mailMessage.setTo(mailTo);
+        mailMessage.setSubject(subject);
+        mailMessage.setText(message);
+
+        mailSender.send(mailMessage);
+    }
+}
